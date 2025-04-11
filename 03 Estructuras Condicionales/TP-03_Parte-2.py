be4640aad03293ade243a3e0c765ff5ac6c059d4
Adrian Brinoccoli <adrianbrinoccoli@gmail.com>
2025-04-11 18:31:04 -0300

    Se agrega ejercicio 7

--- 03 Estructuras Condicionales/TP-03_Parte-2.py
+++ 03 Estructuras Condicionales/TP-03_Parte-2.py
@@ -24,3 +24,13 @@ elif media == mediana and mediana == moda:
 else:
     print(" UPPS! Error")
 
+#7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado termina con vocal,
+# añadir un signo de exclamación al final e imprimir el string resultante por pantalla;}
+# en caso contrario, dejar el string tal cual lo ingreso el usuario e imprimirlo por pantalla.
+
+cadena = str(" Ingresa una frase o palabra ")
+vocales = ["a", "e", "i", "o", "u"]
+if cadena[-1:].lower()in vocales:
+    print(f"[cadena]!")
+else:
+    print(f" cadena")
