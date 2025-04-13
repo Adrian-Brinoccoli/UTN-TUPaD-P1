2a4136c906fc2991506f79039c95304ec5bdc5b5
Adrian Brinoccoli <adrianbrinoccoli@gmail.com>
2025-04-12 22:07:22 -0300

    Se agrega ejercicio 6 y 7

--- 04 Estructuras Repetitivas/BRIÑOCCOLI_TP-04.py
+++ 04 Estructuras Repetitivas/BRIÑOCCOLI_TP-04.py
@@ -39,3 +39,33 @@ while num != 0:
      num = int(input(" Ingrese un numero: "))
 
 print(" Total acumulado:", sumador)
+
+#5) Crea un juego en el que el usuario deba adivinar un numero aleatorio entre 0 y 9. Al final,
+# el programa debe mostrar cuantos intentos fueron necesarios para acertar el número.
+
+import random
+num_aleatorio = random.randint(0, 9)
+
+num_user = int(input(" Intenta adivinar el numero: "))
+intentos = 0
+
+while num_user != num_aleatorio:
+    num_user = int(input(" Intenta de nuevo "))
+    intentos += 1
+
+print(f" Felicitaciones! Acertaste, Te llevo  {intentos} intentos/s")
+
+#6) Desarrolla un programa que imprima en pantalla todos los numeros pares comprendidos entre 0 y 100, en orden decreciente.
+
+for num in range(0+2, 101, 2):
+     print(num)
+
+#7) Crea un programa que calcule la suma de todos los numeros comprendidos entre 0 y un numero entero positivo indicado por el usuario.
+
+numero = int(input(" Ingrese, por favor un numero positivo "))
+suma = 0
+
+for i in range(0, numero +1):
+     suma += i
+
+print(f" La suma total es de {suma} ")
