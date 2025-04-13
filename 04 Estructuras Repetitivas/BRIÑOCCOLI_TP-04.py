4d03efe380c55ca40d6ab726ab5546add2687c7e
Adrian Brinoccoli <adrianbrinoccoli@gmail.com>
2025-04-12 21:32:30 -0300

    Se agregan los ejercicios 1 y 2

---
+++ 04 Estructuras Repetitivas/BRIÑOCCOLI_TP-04.py
@@ -0,0 +1,5 @@
+# Crea un programa que imprima en pantalla todos los numeros enteros desde 0 hasta 100
+#(incluyendo ambos extremos), en orden creciente, mostrando un numero por linea.
+
+for i in range(1,101):
+    print(i, '\t')
