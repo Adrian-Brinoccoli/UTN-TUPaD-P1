0e5b190800feffb97cdb98cb4519a79959550c02
Adrian Briñoccoli <adrianbrinoccoli@gmail.com>
2025-05-27 18:43:27 -0300

    ejercicio 1

---
+++ 05 Funciones/BRIÑOCCOLI_TP-06.py
@@ -0,0 +1,6 @@
+#Crear una funcion llamada imprimir_hola_mundo que imprima por pantalla el mensaje: "Hola Mundo".
+def imprimir_hola_mundo():
+    print("Hola Mundo")
+# Llamada a la funcion desde el programa principal
+if __name__ == "__main__":
+    imprimir_hola_mundo()
